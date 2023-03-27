abd5a67e0fb08bfbc3b9fea72be290932d06dacd
[mimimingfei] <[mingfeiwang950@gmail.com]>
2023-03-27 02:04:33 +0100

    tests fail

--- BackEnd/tests/login.test.js
+++ BackEnd/tests/login.test.js
@@ -1,56 +1,50 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../server.js' 
+import app from '../server.js'
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-  describe('POST /login', () => {
-    it('should return a token for valid credentials', (done) => {
-      const user = {
-        email: 'john.doe@example.com',
-        password: 'password123',
-      };
+describe('POST /login', () => {
+    it('should return a token for valid credentials', async () => {
+        const mockUser = {
+            email: 'john.doe@example.com',
+            password: 'password123',
+        };
 
-      chai.request(app)
-        .post('/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.have.property('token');
-          done();
-        });
+        const res = await chai.request(app)
+            .post('/login')
+            .send(mockUser)
+
+        expect(res).to.have.status(200);
+        expect(res.text).to.include('Login successful');
     });
 
-    it('should return an error for invalid email', (done) => {
-      const user = {
+it('should return an error for invalid email', async () => {
+    const mockUser = {
         email: 'email@example.com',
         password: 'password123',
-      };
+    };
 
-      chai.request(app)
+    const res = await chai.request(app)
         .post('/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res).to.have.status(401);
-          expect(res.body).to.have.property('error').equal('Invalid email or password');
-          done();
-        });
-    });
+        .send(mockUser)
 
-    it('should return an error for invalid password', (done) => {
-      const user = {
+    expect(res).to.have.status(401);
+    expect(res.body).to.have.property('error').equal('Invalid email or password');
+});
+
+
+it('should return an error for invalid password', async () => {
+    const mockUser = {
         email: 'john.doe@example.com',
         password: 'password',
-      };
+    };
 
-      chai.request(app)
+    const res = await chai.request(app)
         .post('/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res).to.have.status(401);
-          expect(res.body).to.have.property('error').equal('Invalid email or password');
-          done();
-        });
-    });
-  });
+        .send(mockUser)
+    expect(res).to.have.status(401);
+    expect(res.body).to.have.property('error').equal('Invalid email or password');
+});
+});
---
+++ BackEnd/tests/mockUsersData.js
@@ -0,0 +1,22 @@
+export const mockUsersData = {
+    user: [{
+        "firstName": "Art",
+        "lastName": "Nelson",
+        "email": "artnelson0@gamil.com",
+        "password": "ow7378rDFxs"
+      }, {
+        "firstName": "Bernard",
+        "lastName": "Ricardo",
+        "email": "bricardo1@hotmail.com",
+        "password": "wtj3736HAvY"
+      }, {
+        "firstName": "Base",
+        "lastName": "Odin",
+        "email": "baseodin2@webs.com",
+        "password": "oE4948ru3NR1"
+      }, {
+        "firstName": "Susannah",
+        "lastName": "Voe",
+        "email": "svlasov4@about.com",
+        "password": "guM17537"
+      }, ]}
